8c441e2c2d2f889f167db5451bebd18cfce978d3
Damian <damian.strony@gmail.com>
2015-10-04 18:06:07 +0200

    change menu position and remove edit in buttons

--- single_pages/dashboard/event_calendar/settings.php
+++ single_pages/dashboard/event_calendar/settings.php
@@ -1,27 +1,22 @@
 <?php defined('C5_EXECUTE') or die('Access denied.'); ?>
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Event Calendar')); ?>
 
-	<div class="dsMenu">
-		<div class="btn-toolbar">
-			<div class="btn-group">
-				<a class="btn btn-primary" href="<?php echo View::url('dashboard/event_calendar/list_calendar') ?>"><?php echo t('Calendars list'); ?>&nbsp;/&nbsp;<?php echo t('Manage events'); ?></a>
-			</div>
-			<div class="btn-group">
-				<a class="btn btn-success" href="<?php echo View::url('dashboard/event_calendar/calendar') ?>"><?php echo t('Add / edit calendar'); ?></a>
-				<a class="btn btn-success" href="<?php echo View::url('dashboard/event_calendar/event') ?>"><?php echo t('Add / edit event'); ?></a>
-			</div>
-			<div class="btn-group">
-				<a class="btn btn-default" href="<?php echo View::url('dashboard/event_calendar/types') ?>"><?php echo t('Type of events'); ?></a>
-				<a class="btn btn-default" href="<?php echo View::url('dashboard/event_calendar/settings') ?>"><?php echo t('Settings'); ?></a>
-			</div>
+	<div class="ccm-dashboard-header-buttons">
+		<div class="btn-group">
+			<a class="btn btn-primary" href="<?php echo View::url('dashboard/event_calendar/list_calendar') ?>"><?php echo t('Calendars list'); ?>&nbsp;/&nbsp;<?php echo t('Manage events'); ?></a>
+		</div>
+		<div class="btn-group">
+			<a class="btn btn-success" href="<?php echo View::url('dashboard/event_calendar/calendar') ?>"><?php echo t('Add calendar'); ?></a>
+			<a class="btn btn-success" href="<?php echo View::url('dashboard/event_calendar/event') ?>"><?php echo t('Add event'); ?></a>
+		</div>
+		<div class="btn-group">
+			<a class="btn btn-default" href="<?php echo View::url('dashboard/event_calendar/types') ?>"><?php echo t('Type of events'); ?></a>
+			<a class="btn btn-default" href="<?php echo View::url('dashboard/event_calendar/settings') ?>"><?php echo t('Settings'); ?></a>
 		</div>
 	</div>
 
 <div class="row">
 	<div class="col-xs-6">
-		<h3><?php echo t('Settings') ?></h3>
-
-
 
 		<form class="form-horizontal" method="post" id="ccm-multilingual-page-report-form" style="margin-top: 15px;">
 
